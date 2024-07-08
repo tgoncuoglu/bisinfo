67222ac5cdab1def0dbb917693b6a19844129821
Tuncay Goncuoglu <tgoncuoglu@gmail.com>
2024-07-08 22:06:27 +0300

    .

--- BISInfo.lua
+++ BISInfo.lua
@@ -5,11 +5,11 @@ function f:OnEvent(event, ...)
 end
 
 function f:ADDON_LOADED(event, addOnName)
-	print("BIS Info loaded. To check your gear use: /bis")
 	-- print(event, addOnName) -- ADDON_LOADED  Blizzard_MajorFactions
 end
 
 function f:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
+	print("BIS Info loaded. To check your gear use: /bis")
 	-- print(event, isLogin, isReload)      -- PLAYER_ENTERING_WORLD false true
 end
 
