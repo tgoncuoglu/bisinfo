b158c20c5cc702af3c8a6eae6dda34057e4b1e65
Tuncay Goncuoglu <tgoncuoglu@gmail.com>
2025-03-20 12:27:08 +0300

    add source info

--- BiSInfoData_Monk.lua
+++ BiSInfoData_Monk.lua
@@ -1,35 +1,35 @@
 BiSData["monk"] = {
     brewmaster = {
-        Head = { id = 229298, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Mane" },
-        Neck = { id = 221103, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Flickering Glowtorc" },
-        Shoulder = { id = 229296, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Shoulderpads" },
-        Back = { id = 199921, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Trashmaster's Mantle" },
-        Chest = { id = 235417, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Venture Co. Plenipotentiary Vest" },
-        Wrist = { id = 158353, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Servo-Arm Bindings" },
-        Hands = { id = 229299, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Handguards" },
-        Waist = { id = 168986, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Mad King's Sporran" },
-        Legs = { id = 229297, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Leggings" },
-        Feet = { id = 159336, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Mercenary Miner's Boots" },
-        Finger0 = { id = 178872, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Ring of Perpetual Conflict" },
-        Trinket0 = { id = 219297, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Cinderbrew Stein" },
-        MainHand = { id = 178864, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Gorebound Predator's Gavel" }
+        Head = { id = 229298, dropsfrom = "The Catalyst,The Great Vault", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Mane" },
+        Neck = { id = 221103, dropsfrom = "Darkflame Cleft", bonus = "657:12042:5878:7981:11996", name = "Flickering Glowtorc" },
+        Shoulder = { id = 229296, dropsfrom = "The Catalyst,The Great Vault", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Shoulderpads" },
+        Back = { id = 199921, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Trashmaster's Mantle" },
+        Chest = { id = 235417, dropsfrom = "The MOTHERLODE!!", bonus = "657:12042:5878:7981:11996", name = "Venture Co. Plenipotentiary Vest" },
+        Wrist = { id = 158353, dropsfrom = "The MOTHERLODE!!", bonus = "657:12042:5878:7981:11996", name = "Servo-Arm Bindings" },
+        Hands = { id = 229299, dropsfrom = "The Catalyst,The Great Vault", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Handguards" },
+        Waist = { id = 168986, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Mad King's Sporran" },
+        Legs = { id = 229297, dropsfrom = "The Catalyst,The Great Vault", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Leggings" },
+        Feet = { id = 159336, dropsfrom = "The MOTHERLODE!!", bonus = "657:12042:5878:7981:11996", name = "Mercenary Miner's Boots" },
+        Finger0 = { id = 178872, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Ring of Perpetual Conflict" },
+        Trinket0 = { id = 219297, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Cinderbrew Stein" },
+        MainHand = { id = 178864, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Gorebound Predator's Gavel" }
     },
     mistweaver = {
 
     },
     windwalker = {
-        Head = { id = 229298, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Mane" },
-        Neck = { id = 221060, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Strapped Rescue-Keg" },
-        Shoulder = { id = 229296, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Shoulderpads" },
-        Back = { id = 234507, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Electrician's Siphoning Filter" },
-        Chest = { id = 229301, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Inked Coils" },
-        Wrist = { id = 221053, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Battle-Scarred Fisticuffs" },
-        Hands = { id = 229299, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Handguards" },
-        Waist = { id = 168957, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Mekgineer's Championship Belt" },
-        Legs = { id = 168968, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Flame-Seared Leggings" },
-        Feet = { id = 221071, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Backbreaking Bootstrappers" },
-        Finger0 = { id = 178869, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Fleshfused Circle" },
-        MainHand = { id = 221051, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Crusher of the Unsettled" },
-        Trinket0 = { id = 219308, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Signet of the Priory" }
+        Head = { id = 229298, dropsfrom = "Catalyst", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Mane" },
+        Neck = { id = 221060, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Strapped Rescue-Keg" },
+        Shoulder = { id = 229296, dropsfrom = "Catalyst", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Shoulderpads" },
+        Back = { id = 234507, dropsfrom = "Operation: Floodgate", bonus = "657:12042:5878:7981:11996", name = "Electrician's Siphoning Filter" },
+        Chest = { id = 229301, dropsfrom = "Catalyst", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Inked Coils" },
+        Wrist = { id = 221053, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Battle-Scarred Fisticuffs" },
+        Hands = { id = 229299, dropsfrom = "Catalyst", bonus = "7981:12042:5878:11996", name = "Ageless Serpent's Handguards" },
+        Waist = { id = 168957, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Mekgineer's Championship Belt" },
+        Legs = { id = 168968, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Flame-Seared Leggings" },
+        Feet = { id = 221071, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Backbreaking Bootstrappers" },
+        Finger0 = { id = 178869, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Fleshfused Circle" },
+        MainHand = { id = 221051, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Crusher of the Unsettled" },
+        Trinket0 = { id = 219308, dropsfrom = "Priory of the Sacred Flame", bonus = "657:12042:5878:7981:11996", name = "Signet of the Priory" }
     }
 }
--- BiSInfoData_Warrior.lua
+++ BiSInfoData_Warrior.lua
@@ -1,37 +1,37 @@
 BiSData["warrior"] = {
     arms = {
-        Head = { id = 221100, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Waxsteel Greathelm" },
-        Shoulder = { id = 159415, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Skyscorcher Pauldrons" },
-        Chest = { id = 221069, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Slashproof Business Plate" },
-        Hands = { id = 168980, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Gauntlets of Absolute Authority" },
-        Waist = { id = 234505, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Venture Contractor's Floodlight" },
-        Legs = { id = 178800, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Galvanized Oxxein Legguards" },
-        Feet = { id = 159679, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Sabatons of Rampaging Elements" },
-        Finger0 = { id = 178872, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Ring of Perpetual Conflict" }
+        Head = { id = 221100, dropsfrom = "Darkflame Cleft", bonus = "657:12042:5878:7981:11996", name = "Waxsteel Greathelm" },
+        Shoulder = { id = 159415, dropsfrom = "The MOTHERLODE!!", bonus = "657:12042:5878:7981:11996", name = "Skyscorcher Pauldrons" },
+        Chest = { id = 221069, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Slashproof Business Plate" },
+        Hands = { id = 168980, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Gauntlets of Absolute Authority" },
+        Waist = { id = 234505, dropsfrom = "Operation: Floodgate", bonus = "657:12042:5878:7981:11996", name = "Venture Contractor's Floodlight" },
+        Legs = { id = 178800, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Galvanized Oxxein Legguards" },
+        Feet = { id = 159679, dropsfrom = "The MOTHERLODE!!", bonus = "657:12042:5878:7981:11996", name = "Sabatons of Rampaging Elements" },
+        Finger0 = { id = 178872, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Ring of Perpetual Conflict" }
     },
     fury = {
-        Head = { id = 169003, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Apogee Inventor's Goggles" },
-        Shoulder = { id = 221052, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Foam-Ridden Pauldrons" },
-        Chest = { id = 168979, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Mechanized Plate Chasse" },
-        Hands = { id = 234504, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Jumpstarter's Scaffold-Scrapers" },
-        Waist = { id = 221040, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Lightning-Conductor's Bands" },
-        Legs = { id = 178800, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Galvanized Oxxein Legguards" },
-        Feet = { id = 221061, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Hops-Laden Greatboots" },
-        Finger0 = { id = 221198, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "85-Year Tenure Ring" }
+        Head = { id = 169003, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Apogee Inventor's Goggles" },
+        Shoulder = { id = 221052, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Foam-Ridden Pauldrons" },
+        Chest = { id = 168979, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Mechanized Plate Chasse" },
+        Hands = { id = 234504, dropsfrom = "Operation: Floodgate", bonus = "657:12042:5878:7981:11996", name = "Jumpstarter's Scaffold-Scrapers" },
+        Waist = { id = 221040, dropsfrom = "The Rookery", bonus = "657:12042:5878:7981:11996", name = "Lightning-Conductor's Bands" },
+        Legs = { id = 178800, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Galvanized Oxxein Legguards" },
+        Feet = { id = 221061, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Hops-Laden Greatboots" },
+        Finger0 = { id = 221198, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "85-Year Tenure Ring" }
     },
     protection = {
-        Head = { id = 229235, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Faceshield" },
-        Neck = { id = 221060, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Strapped Rescue-Keg" },
-        Shoulder = { id = 229233, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Shoulderplates" },
-        Back = { id = 199921, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Trashmaster's Mantle" },
-        Chest = { id = 221069, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Slashproof Business Plate" },
-        Wrist = { id = 222435, dropsfrom = "", bonus = "12040:1515:8790", name = "Everforged Vambraces" },
-        Hands = { id = 229236, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Crushers" },
-        Waist = { id = 234505, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Venture Contractor's Floodlight" },
-        Legs = { id = 229234, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Chausses" },
-        Feet = { id = 159679, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Sabatons of Rampaging Elements" },
-        Finger0 = { id = 221099, dropsfrom = "", bonus = "7981:12042:5878:11996", name = "Wick's Golden Loop" },
-        MainHand = { id = 169608, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Tearing Sawtooth Blade" },
-        SecondaryHand = { id = 178867, dropsfrom = "", bonus = "657:12042:5878:7981:11996", name = "Barricade of the Endless Empire" }
+        Head = { id = 229235, dropsfrom = "Catalyst / Raid / Vault", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Faceshield" },
+        Neck = { id = 221060, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Strapped Rescue-Keg" },
+        Shoulder = { id = 229233, dropsfrom = "Catalyst / Raid / Vault", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Shoulderplates" },
+        Back = { id = 199921, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Trashmaster's Mantle" },
+        Chest = { id = 221069, dropsfrom = "Cinderbrew Meadery", bonus = "657:12042:5878:7981:11996", name = "Slashproof Business Plate" },
+        Wrist = { id = 222435, dropsfrom = "Blacksmithing", bonus = "12040:1515:8790", name = "Everforged Vambraces" },
+        Hands = { id = 229236, dropsfrom = "Catalyst / Raid / Vault", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Crushers" },
+        Waist = { id = 234505, dropsfrom = "Operation: Floodgate", bonus = "657:12042:5878:7981:11996", name = "Venture Contractor's Floodlight" },
+        Legs = { id = 229234, dropsfrom = "Catalyst / Raid / Vault", bonus = "7981:12042:5878:11996", name = "Enforcer's Backalley Chausses" },
+        Feet = { id = 159679, dropsfrom = "The MOTHERLODE!!", bonus = "657:12042:5878:7981:11996", name = "Sabatons of Rampaging Elements" },
+        Finger0 = { id = 221099, dropsfrom = "Darkflame Cleft", bonus = "7981:12042:5878:11996", name = "Wick's Golden Loop" },
+        MainHand = { id = 169608, dropsfrom = "Operation: Mechagon Workshop", bonus = "657:12042:5878:7981:11996", name = "Tearing Sawtooth Blade" },
+        SecondaryHand = { id = 178867, dropsfrom = "Theater of Pain", bonus = "657:12042:5878:7981:11996", name = "Barricade of the Endless Empire" }
     }
 }
